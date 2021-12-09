a8dbbba8971d2976306ff4e4187500e9f677c593
Serhii T <tocarchuk@gmail.com>
2021-12-09 23:39:08 +0200

    Add Comment model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts
+  has_many :comments
   validates :username, presence: true, uniqueness: true, length: {minimum: 3, maximum: 20}
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: {minimum: 7, maximum: 15}
