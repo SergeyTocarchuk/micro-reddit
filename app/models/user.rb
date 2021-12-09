58bdf4d94e914d98b76ae883bb3824171368c19b
Serhii T <tocarchuk@gmail.com>
2021-12-09 23:21:20 +0200

    Add Post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true, length: {minimum: 3, maximum: 20}
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: {minimum: 7, maximum: 15}
 end
